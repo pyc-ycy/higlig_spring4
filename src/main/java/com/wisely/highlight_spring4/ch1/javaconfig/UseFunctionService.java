ca2c2103ac2c9ceafef91718c224e60850081e96
御承扬 <2923616405@qq.com>
2019-09-22 17:25:18 +0800

    Update

---
+++ src/main/java/com/wisely/highlight_spring4/ch1/javaconfig/UseFunctionService.java
@@ -0,0 +1,12 @@
+package com.wisely.highlight_spring4.ch1.javaconfig;
+import com.wisely.highlight_spring4.ch1.javaconfig.FunctionService;
+
+public class UseFunctionService {
+    FunctionService functionService;
+    public void setFunctionService(FunctionService functionService){
+        this.functionService = functionService;
+    }
+    public String SayHello(String word){
+        return functionService.sayHello(word);
+    }
+}
