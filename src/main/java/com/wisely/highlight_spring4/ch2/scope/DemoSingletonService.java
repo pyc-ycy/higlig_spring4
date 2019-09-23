cdd96db416779ac9e5cb4903c6820aae74d5a9d7
御承扬 <2923616405@qq.com>
2019-09-23 15:27:22 +0800

    Update

---
+++ src/main/java/com/wisely/highlight_spring4/ch2/scope/DemoSingletonService.java
@@ -0,0 +1,5 @@
+package com.wisely.highlight_spring4.ch2.scope;
+import org.springframework.stereotype.Service;
+@Service
+public class DemoSingletonService {
+}
---
+++ src/main/java/com/wisely/highlight_spring4/ch2/scope/ScopeConfig.java
@@ -0,0 +1,16 @@
+// IntelliJ IDEA
+// higlig_spring4
+//ScopeConfig
+// 彭友聪
+//2019/09/23
+// Author:御承扬
+//E-mail:2923616405@qq.com
+
+
+package com.wisely.highlight_spring4.ch2.scope;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+@ComponentScan("com.wisely.highlight_spring4.ch2.scope")
+public class ScopeConfig {
+}
